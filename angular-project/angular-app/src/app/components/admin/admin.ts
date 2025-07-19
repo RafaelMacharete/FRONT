02aeaa3e536c7f09726f7b48beb233dbc4645a2b
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-07-19 08:09:32 -0300

    feat: components (admin & user)

---
+++ angular-project/angular-app/src/app/components/admin/admin.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  imports: [],
+  templateUrl: './admin.html',
+  styleUrl: './admin.css'
+})
+export class Admin {
+
+  firstName = 'Rafael';
+
+  courseName = 'System Development';
+
+  age = 17;
+}
