a00cdd3ee5f27c3ba52f7cf41d024aab65b47baf
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-07-21 23:10:01 -0300

    feat: get http req

--- angular-project/angular-app/src/app/components/get-api/get-api.ts
+++ angular-project/angular-app/src/app/components/get-api/get-api.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-get-api',
@@ -6,6 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './get-api.html',
   styleUrl: './get-api.css'
 })
-export class GetApi {
+export class GetApi implements OnInit {
+  http = inject(HttpClient); // constructor injection
+  userList: any[] = [];
 
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((res: any) => {
+      this.userList = res;
+    })
+  }
 }
