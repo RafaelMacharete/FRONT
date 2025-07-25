59fa3659ac8dec95a4a0bb4e664a2cb266524c28
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-07-24 23:34:46 -0300

    feat: reactive

---
+++ angular-project/angular-app/src/app/components/reactive-user/reactive-user.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-user',
+  imports: [ReactiveFormsModule],
+  templateUrl: './reactive-user.html',
+  styleUrl: './reactive-user.css'
+})
+export class ReactiveUser implements OnInit{
+
+  userList: any[] = [];
+  http = inject(HttpClient);
+
+  userForm: FormGroup = new FormGroup();
+  
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.http.get('https://api.freeprojectapi.com/api/GoalTracker/getAll').subscribe({
+    })
+  }
+}
