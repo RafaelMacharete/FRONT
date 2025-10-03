a42b86f97e4e3491c171198c507c8d00624cf5f0
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-10-03 14:31:16 -0300

    saep finished

--- saep/backend/task/models.py
+++ saep/backend/task/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(unique=True   )
 
     def __str__(self):
         return self.username
